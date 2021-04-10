2064a94a4fbcaed315d96067438896e89f11428e
Jonathan Philips <johnphilips.995@gmail.com>
2021-04-10 21:10:01 +0400

    Create SPGL Minimum Example.py

---
+++ Missile Command/SPGL Minimum Example.py
@@ -0,0 +1,20 @@
+import spgl
+
+# Create Classes
+
+# Create Functions
+
+# Initial Game setup
+game = spgl.Game(800, 600, "black", "SPGL Minimum Code Example by Jonathan")
+
+# Create Sprites
+
+# Create Labels
+
+# Create Buttons
+
+# Set Keyboard Bindings
+
+while True:
+    # Call the game tick method
+    game.tick()
