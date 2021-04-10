1bcbbfc796781ad54544fe74b6e18e055bc4ab2d
Jonathan Philips <johnphilips.995@gmail.com>
2021-04-10 19:44:34 +0400

    Create settings.py

---
+++ Pac-Man/settings.py
@@ -0,0 +1,21 @@
+from pygame.math import Vector2 as vec
+
+# screen settings
+WIDTH, HEIGHT = 610, 670
+FPS = 60
+TOP_BOTTOM_BUFFER = 50
+MAZE_WIDTH, MAZE_HEIGHT = WIDTH-TOP_BOTTOM_BUFFER, HEIGHT-TOP_BOTTOM_BUFFER
+
+ROWS = 30
+COLS = 28
+
+# colour settings
+BLACK = (0, 0, 0)
+RED = (208, 22, 22)
+GREY = (107, 107, 107)
+WHITE = (255, 255, 255)
+PLAYER_COLOUR = (190, 194, 15)
+
+# font settings
+START_TEXT_SIZE = 64
+START_FONT = 'commodore 64 pixelized'
