279804a458954a8a8379989a754405afed226b0b
Jonathan Philips <johnphilips.995@gmail.com>
2023-01-18 12:19:38 +0400

    Update Number-Guesser.py

--- Number-Guesser.py
+++ Number-Guesser.py
@@ -1,22 +1,17 @@
-import random
-import math
+import random, math
 
-lower_limit = int(input("Enter Lower Limit Number:- "))
-upper_limit = int(input("Enter Upper Limit Number:- "))
+lower_limit,upper_limit=int(input("Enter Lower Limit: ")),int(input("Enter Upper Limit: "))
 
-x = random.randint(lower_limit, upper_limit)
-print("\n\tYou've only ",round(math.log(upper_limit - lower_limit + 1, 2))," chances to guess the integer!\n")
+x=random.randint(lower_limit, upper_limit)
+print("\n\tYou have only ",round(math.log(upper_limit-lower_limit+1,2))," chances to guess the integer!\n")
 
 count = 0
 
 while count < math.log(upper_limit - lower_limit + 1, 2):
     count += 1
-    guess = int(input("Guess a number:- "))
-    if x == guess and count == 1:
-        print("Congratulations! You did it in 1 try!")
-        break
-    if x == guess and count > 1:
-        print("Congratulations! You did it in ",count," tries!")
+    guess = int(input("Guess a number: "))
+    if x == guess:
+        print("Congratulations! You did it in",count,"tries!")
         break
     elif x > guess:
         print("You guessed too small!")
