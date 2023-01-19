0c5dbf4a2b05a5e82efa23625e55df2492c95714
Jonathan Philips <johnphilips.995@gmail.com>
2023-01-19 11:23:28 +0400

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,20 +1,10 @@
-count = 0
-
-for i in range(1, 101):
-    if i % 5 == 0 and i % 3 == 0:
+lolim,uplim=int(input('Enter lower limit: ')),int(input('Enter upper limit: '))
+for i in range(lolim,uplim+1):
+    if i%5==0 and i%3==0:
         print("FizzBuzz")
-        count += 1
-
-    elif i % 3 == 0:
+    elif i%3==0:
         print("Fizz")
-        count += 1
-    
-    elif i % 5 == 0:
+    elif i%5==0:
         print("Buzz")
-        count += 1
-
     else:
         print(i)
-        count += 1
-
-print(f"The number of comparisons done is:", count)
