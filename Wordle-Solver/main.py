65dfcc30a4e26b9a457830f4327f5465cb73a7e7
Jonathan Philips <johnphilips.995@gmail.com>
2023-12-05 20:53:42 +0530

    Formatted code

--- Wordle-Solver/main.py
+++ Wordle-Solver/main.py
@@ -1,32 +1,33 @@
-words=[]
-with open('Python-Projects\Wordle-Solver\words.txt') as f:
+words = []
+with open("Python-Projects\\Wordle-Solver\\words.txt") as f:
     for line in f:
         words.append(line.strip())
 
-def play_wordle(words : list) -> None:
+
+def play_wordle(words: list) -> None:
     while len(words) > 1:
         letters_in_word = []
         for i in range(5):
-            letter = input("Letter in position " + str(i+1) +": ").lower()
+            letter = input("Letter in position " + str(i + 1) + ": ").lower()
             color = input("What color is it (Green, Yellow, Grey): ").lower()
-            if color != 'green' and color != 'yellow' and color != 'grey':
+            if color != "green" and color != "yellow" and color != "grey":
                 print("Invalid input...")
                 return
             words_to_remove = []
             for word in words:
-                if color == 'green':
+                if color == "green":
                     if word[i] != letter:
                         words_to_remove.append(word)
                         if letter not in letters_in_word:
                             letters_in_word.append(letter)
-                elif color == 'yellow':
+                elif color == "yellow":
                     if letter not in word:
                         words_to_remove.append(word)
                     elif word[i] == letter:
                         words_to_remove.append(word)
                     if letter not in letters_in_word:
                         letters_in_word.append(letter)
-                elif color == 'grey':
+                elif color == "grey":
                     if letter in word:
                         if letter not in letters_in_word:
                             words_to_remove.append(word)
@@ -36,7 +37,9 @@ def play_wordle(words : list) -> None:
                 words.remove(word)
         print(words)
         guess = input("Did you guess correctly (Y/N)?: ").lower()
-        if guess == 'y':
+        if guess == "y":
             print("Congratulations! You win!")
             return
+
+
 play_wordle(words)
